582cb8f0bc07bbe3806d9d5c2b5eee8750f5309c
Cristian Marin <marin101998@gmail.com>
2024-07-02 15:35:58 -0500

    + Update | v10 02-07-24 extends crud and update routes

--- Http/Requests/UpdateMenuRequest.php
+++ Http/Requests/UpdateMenuRequest.php
@@ -8,11 +8,8 @@ class UpdateMenuRequest extends FormRequest
 {
     public function rules()
     {
-        $menu = $this->route()->parameter('menu');
-
         return [
             'name' => 'required',
-            'primary' => "unique:menu__menus,primary,{$menu->id}",
         ];
     }
 
@@ -25,7 +22,6 @@ class UpdateMenuRequest extends FormRequest
     {
         return [
             'name.required' => trans('menu::validation.name is required'),
-            'primary.unique' => trans('menu::validation.only one primary menu'),
         ];
     }
 }
--- Transformers/MenuTransformer.php
+++ Transformers/MenuTransformer.php
@@ -3,38 +3,21 @@
 namespace Modules\Menu\Transformers;
 
 use Illuminate\Support\Facades\Cache;
-use Modules\Ihelpers\Transformers\BaseApiTransformer;
-use Modules\Isite\Transformers\RevisionTransformer;
+use Modules\Core\Icrud\Transformers\CrudResource;
 
-class MenuTransformer extends BaseApiTransformer
+class MenuTransformer extends CrudResource
 {
-    public function toArray($request)
+
+    /**
+     * Method to merge values with response
+     *
+     * @return array
+     */
+    public function modelAttributes($request)
     {
-        $data = [
-            'id' => $this->when($this->id, $this->id),
-            'name' => $this->when($this->name, $this->name),
-            'title' => $this->when($this->title, $this->title),
-            'primary' => $this->when($this->primary, $this->primary),
-            'isDefault' => $this->when(isset($this->is_default), $this->is_default),
-            'status' => $this->when(isset($this->status), $this->status),
-            'createdAt' => $this->when($this->created_at, $this->created_at),
-            'updatedAt' => $this->when($this->updated_at, $this->updated_at),
-            //'menuitems' => MenuitemTransformer::collection($this->whenLoaded('menuitems'))
+        return [
             'menuitems' => $this->getMenuItems(),
-            'revisions' => RevisionTransformer::collection($this->whenLoaded('revisions')),
         ];
-
-        $filter = json_decode($request->filter);
-        if (isset($filter->allTranslations) && $filter->allTranslations) {
-            // Get langs avaliables
-            $languages = \LaravelLocalization::getSupportedLocales();
-            foreach ($languages as $lang => $value) {
-                $data[$lang]['title'] = $this->hasTranslation($lang) ? $this->translate("$lang")['title'] : '';
-                $data[$lang]['status'] = $this->hasTranslation($lang) ? $this->translate("$lang")['status'] : '';
-            }
-        }
-
-        return $data;
     }
 
     /*
@@ -42,7 +25,7 @@ class MenuTransformer extends BaseApiTransformer
     */
     public function getMenuItems()
     {
-        return Cache::store(config('cache.default'))->remember('menu_items_'.$this->id.(tenant()->id ?? ''), 60, function () {
+        return Cache::store(config('cache.default'))->remember('menu_items_' . $this->id . (tenant()->id ?? ''), 60, function () {
             $params = [
                 'include' => [],
                 'filter' => [
@@ -53,7 +36,7 @@ class MenuTransformer extends BaseApiTransformer
 
             $menuItems = app('Modules\Menu\Repositories\MenuItemRepository')->getItemsBy(json_decode(json_encode($params)));
 
-            if (! empty($menuItems)) {
+            if (!empty($menuItems)) {
                 return MenuitemTransformer::collection($menuItems);
             }
 
