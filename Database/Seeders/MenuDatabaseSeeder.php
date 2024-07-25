bdb564ccfb83a542cae6cedc61477a9db458036d
msolanogithub <msolano@imaginacolombia.com>
2024-07-25 15:31:54 -0500

    Remove the cms records

--- Database/Seeders/MenuDatabaseSeeder.php
+++ Database/Seeders/MenuDatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Modules\Menu\Database\Seeders;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
 use Modules\Menu\Database\Seeders\CMSSidebarDatabaseSeeder;
+use Modules\Page\Database\Seeders\DeleteCMSPagesDatabaseSeeder;
 
 class MenuDatabaseSeeder extends Seeder
 {
@@ -18,6 +19,7 @@ class MenuDatabaseSeeder extends Seeder
     Model::unguard();
     //Seed cms pages
     $this->call(MenuModuleTableSeeder::class);
+    $this->call(DeleteCMSSidebarDatabaseSeeder::class);
     //$this->call(CMSSidebarDatabaseSeeder::class);
   }
 }
